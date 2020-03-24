0035eb2d39b5c0749cef091dc5091712c19ffd1a
Titas Gailius <titas.gailius@gmail.com>
2020-03-24 10:55:38 +0100

    Merge data by default

--- src/Builder.php
+++ src/Builder.php
@@ -195,35 +195,7 @@ class Builder
      */
     public function with($key, $value = null)
     {
-        if (is_array($key)) {
-            return $this->setWith($key);
-        }
-
-        return $this->mergeWith([$key => $value]);
-    }
-
-    /**
-     * Set command data bindings.
-     *
-     * @param array  $with
-     * @return $this
-     */
-    public function setWith(array $with)
-    {
-        $this->with = $with;
-
-        return $this;
-    }
-
-    /**
-     * Merge command bindings.
-     *
-     * @param  array  $with
-     * @return $this
-     */
-    public function mergeWith(array $with)
-    {
-        $this->with = array_merge($this->with, $with);
+        $this->with = array_merge($this->with, is_array($key) ? $key : [$key => $value]);
 
         return $this;
     }
